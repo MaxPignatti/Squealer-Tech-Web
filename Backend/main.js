cad956357276d93eff6c62b70d782b73fc945d84
Lorenzo Rolfini <lorenzo.rolfini@live.it>
2023-09-27 11:50:48 +0200

    Struttura user database

--- Backend/main.js
+++ Backend/main.js
@@ -1,4 +1,17 @@
 const express = require('express')
 var bodyparser = require('body-parser')
 
-const app= express()
+const app = express()
+
+/*CAMPI PER USER:
+Nome
+Cognome
+Foto profilo
+Nome utente
+Email 
+Password
+Caratteri rimanenti (giorno, settimana, mese)
+debito caratteri
+Canali a cui partecipa
+tipo di account
+Social media manager*/

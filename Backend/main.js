674fbe501156c444c01be9836523bde0b2fcd7e1
MaxPignatti <90690709+MaxPignatti@users.noreply.github.com>
2023-09-28 13:35:30 +0200

    Push ergonomico

--- Backend/main.js
+++ Backend/main.js
@@ -1,44 +1,50 @@
-const express = require('express')
-var bodyparser = require('body-parser')
-const app = express()
-var mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express.Router();
 
-/*CAMPI PER USER:
-Nome
-Cognome
-Foto profilo
-Nome utente
-Email 
-Password
-Caratteri rimanenti (giorno, settimana, mese)
-debito caratteri
-Canali a cui partecipa
-tipo di account
-Social media manager*/
+// Connessione a MongoDB
+mongoose.connect("mongodb://localhost:27017/test", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
-mongoose.connect("mongodb://localhost:27017/test", { useUnifiedTopology: true, useNweUrlParser: true })
-const schema = new mongoose.schema({
-    nome: String,
-    cognome: String,
-    foto_profilo: String,
-    nome_utente: String,
-    email: String,
-    password: String,
-    rem_char: Number,
-    deb_char: Number,
-    channel: String,
-    account_type: Number,
-    smm: Boolean
-})
+// Definizione dello schema del documento
+const userSchema = new mongoose.Schema({
+  nome: String,
+});
 
-const Model = mongoose.model("UtenteFantastico", schema);
+// Creazione del modello User basato sullo schema
+const User = mongoose.model("UtenteFantastico", userSchema);
 
-const insertUser = async (obj) => {
-    const user = new Model(obj);
+// Middleware per l'analisi dei dati del corpo della richiesta
+app.use(express.urlencoded({ extended: true }));
 
-    await user.save();
-}
-
-insertUser({
-    
-})
+// Gestione della richiesta POST per la creazione di un nuovo utente
+app.post('/submit-form', async (req, res) => {
+  try {
+    // Salvataggio del documento nel database
+    await User.create(req.body);
+    console.log('User saved successfully');
+    res.send('User saved successfully');
+  } catch (err) {
+    console.error('Error saving user:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+/*
+const schema = new mongoose.Schema({
+  nome: String,
+  cognome: String,
+  foto_profilo: String,
+  nome_utente: String,
+  email: String,
+  password: String,
+  rem_char: Number,
+  deb_char: Number,
+  channel: String,
+  account_type: Number,
+  smm: Boolean
+});
+const User = mongoose.model("UtenteFantastico", schema);
+ */
+module.exports = app;

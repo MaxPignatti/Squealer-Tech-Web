2c1c9325e70cf2c52e9ae6746cd7446950b8f057
MaxPignatti <90690709+MaxPignatti@users.noreply.github.com>
2023-09-28 12:17:03 +0200

    In vantaggio

--- Backend/main.js
+++ Backend/main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var bodyparser = require('body-parser')
-
 const app = express()
+var mongoose = require('mongoose')
 
 /*CAMPI PER USER:
 Nome
@@ -15,3 +15,18 @@ debito caratteri
 Canali a cui partecipa
 tipo di account
 Social media manager*/
+
+mongoose.connect("mongodb://localhost:27017/test", { useUnifiedTopology: true, useNweUrlParser: true })
+const schema = new mongoose.schema({
+    nome: String,
+    cognome: String,
+    foto_profilo: String,
+    nome_utente: String,
+    email: String,
+    password: String,
+    rem_char: Number,
+    deb_char: Number,
+    channel: NodeList,
+    account_type: Number,
+    smm: Boolean
+})

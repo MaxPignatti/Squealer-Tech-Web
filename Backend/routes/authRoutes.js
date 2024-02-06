ff4b1058e69a63cc48e92c10974484ec936c61df
MaxPignatti <maxpignatti@gmail.com>
2024-02-06 12:57:35 +0100

    Risolto :D

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,53 +5,52 @@ const { body, validationResult } = require("express-validator");
 
 // Middleware per controllare i risultati della validazione
 const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	next();
 };
 
 router.post(
-  "/loginSMM",
-  [
-    body("email")
-      .isEmail()
-      .withMessage("Deve essere un'email valida.")
-      .normalizeEmail()
-      .toString(),
-    body("password").trim().escape().toString(),
-  ],
-  validate,
-  authController.loginSMM
+	"/loginSMM",
+	[
+		body("email")
+			.isEmail()
+			.withMessage("Deve essere un'email valida.")
+			.normalizeEmail(),
+		body("password").trim().escape(),
+	],
+	validate,
+	authController.loginSMM
 );
 
 router.post(
-  "/register",
-  [
-    body("username").isString().trim().escape().toString(),
+	"/register",
+	[
+		body("username").isString().trim().escape(),
 
-    body("password").isLength({ min: 5 }).trim().escape().toString(),
+		body("password").isLength({ min: 5 }).trim().escape(),
 
-    body("email").isEmail().normalizeEmail().toString(),
+		body("email").isEmail().normalizeEmail(),
 
-    body("firstName").optional().trim().escape().toString(),
+		body("firstName").optional().trim().escape(),
 
-    body("lastName").optional().trim().escape().toString(),
-  ],
-  validate,
-  authController.register
+		body("lastName").optional().trim().escape(),
+	],
+	validate,
+	authController.register
 );
 
 router.post(
-  "/login",
-  [
-    body("username").isString().trim().escape(),
+	"/login",
+	[
+		body("username").isString().trim().escape(),
 
-    body("password").trim().escape().toString(),
-  ],
-  validate,
-  authController.login
+		body("password").trim().escape(),
+	],
+	validate,
+	authController.login
 );
 
 router.post("/protectedEndpoint", validate, authController.protectedEndpoint);
@@ -59,12 +58,14 @@ router.post("/protectedEndpoint", validate, authController.protectedEndpoint);
 router.get("/verifyTokenSMM", validate, authController.verifyTokenSMM);
 
 router.post(
-  "/loginMod",
-  [
-    body("username").isString().trim().escape(),
+	"/loginMod",
+	[
+		body("username").isString().trim().escape(),
 
-    body("password").trim().escape().toString(),
-  ],
-  validate,
-  authController.loginMod
+		body("password").trim().escape(),
+	],
+	validate,
+	authController.loginMod
 );
+
+module.exports = router;
